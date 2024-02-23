31d73d7416e83883ef87626907c4b879f7a9a85d
John Eikenberry <jae@zhar.net>
2024-02-23 15:06:44 -0800

    initial working main dial loop and testing code
    Refactored main loop to meet requirements and enable testing.
    Used structured logging as standard.
    Handle failed reads/retry.

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"errors"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"net"
 	"os"
 )
@@ -22,33 +23,46 @@ func init() {
 	}
 }
 
-// main loop
 func main() {
 	ctx := SignalContext()
+	output := func(msg message) {
+		fmt.Printf("%+v\n", msg)
+	}
 
-	//go testServer(ctx)
+	dialLoop(ctx, Address, output)
+}
 
+// dialLoop runs the main dial loop connecting to and reading in the data from
+// the server. It is split out for testing.
+func dialLoop(ctx context.Context, addr string, output func(message)) {
 	d := new(net.Dialer)
+	buf := new(bufio.Reader)
 	for {
 		// connect to server via tcp
-		conn, err := d.DialContext(ctx, "tcp", Address)
+		conn, err := d.DialContext(ctx, "tcp", addr)
+		//   retry on conn errors
 		switch {
 		case errors.Is(ctx.Err(), context.Canceled):
 			return // ie. exit (dialer's context cancelled via signal)
 		case err != nil:
-			panic(err)
+			slog.Info("DialContext", "error", err)
+			continue
 		}
 		defer conn.Close()
 		// wait for data
-		//   retry on conn errors
 		// receive data in buffer
-		//   retry on read errors
-		buf, _ := io.ReadAll(conn)
+		buf.Reset(conn)
+		data, err := io.ReadAll(buf)
 		// retry if error before all read
-
+		if err != nil {
+			slog.Info("ReadAll", "error", err)
+			continue
+		}
 		// parse data into struct
+		msg := new(message)
+		msg.UnmarshalBinary(data)
 		// pretty print struct
-		fmt.Println(string(buf))
+		output(*msg)
 		// loop
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,36 +1,59 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"io"
 	"net"
-	"strings"
-	"time"
+	"testing"
 )
 
+func TestDialLoop(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	listener := testServer(cancel, testMessages...)
+
+	results := make(chan message)
+	output := func(msg message) {
+		results <- msg
+	}
+
+	addr := listener.Addr().String()
+	go dialLoop(ctx, addr, output)
+	for _, want := range testMessages {
+		got := <-results
+		if want != got {
+			t.Errorf("message parse error; want: `%#v`, got: `%#v`", want, got)
+		}
+	}
+}
+
 // testServer runs a server to test against
-func testServer(ctx context.Context) {
-	ln, err := new(net.ListenConfig).Listen(ctx, "tcp", Address)
+func testServer(cancel func(), tests ...message) net.Listener {
+	ln, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		panic(err)
 	}
-	defer ln.Close()
 
-	for {
-		conn, err := ln.Accept()
-		switch {
-		case errors.Is(err, net.ErrClosed):
-			return
-		case err != nil:
-			panic(err)
-		}
-
-		// handles unlimited connections
-		go func() {
-			time.Sleep(time.Millisecond * 100)
-			io.Copy(conn, strings.NewReader("hello world"))
+	go func() {
+		defer ln.Close()
+		for _, msg := range tests {
+			conn, err := ln.Accept()
+			switch {
+			case errors.Is(err, context.Canceled):
+				return
+			case err != nil:
+				panic(err)
+			}
+			bin, err := msg.MarshalBinary()
+			if err != nil {
+				panic(err)
+			}
+			io.Copy(conn, bytes.NewReader(bin))
 			conn.Close()
-		}()
-	}
+		}
+		cancel()
+	}()
+	return ln
 }
